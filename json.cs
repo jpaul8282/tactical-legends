c3ca3c9e3f61a8fa4d1758206aa20db5e5efb5a1
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-03 22:44:34 +0200

    Create json.cs
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

---
+++ json.cs
@@ -0,0 +1,19 @@
+{
+  "mission_id": "zone_viper",
+  "base_state": "Extraction",
+  "branches": {
+    "low_casualty": {
+      "next_mission": "CivilianAllySupport",
+      "bonus": "Medical Reinforcement Units"
+    },
+    "high_alert": {
+      "next_mission": "BlackSectorLockdown",
+      "bonus": "Drone Override Tech"
+    },
+    "faction_domination": {
+      "next_mission": "Paramilitary Coup",
+      "bonus": "Stormfront Shock Battalion"
+    }
+  }
+}
+
