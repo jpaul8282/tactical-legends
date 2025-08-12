2afe234c063021194cfa0c33f0613b9debc3685b
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-12 21:35:30 +0200

    Update App.tsx
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

--- app/App.tsx
+++ app/App.tsx
@@ -15,3 +15,23 @@ export default function App() {
     </LiveblocksProvider>
   );
 }
+"Use client";
+
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import { Room } from "./Room";
+
+export default function App() {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_covyb-1-pgYI4blKVeOLwB31adA-Pqqoa6S9tewgCcD4D68WQpFuvxpFpODm-B07"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          <Room />
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
