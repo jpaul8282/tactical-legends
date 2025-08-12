a3a5eb61b8a0611cf7b81071ed4af515a3fc275a
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-12 11:06:29 +0200

    Create Room.tsx
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"Use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_covyb-1-pgYI4blKVeOLwB31adA-Pqqoa6S9tewgCcD4D68WQpFuvxpFpODm-B07"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
