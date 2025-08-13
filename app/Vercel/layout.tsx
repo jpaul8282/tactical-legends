8895e3b583cf521d4661783066c00905a37fd798
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-13 09:56:05 +0200

    Update layout.tsx
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

--- app/Vercel/layout.tsx
+++ app/Vercel/layout.tsx
@@ -17,3 +17,24 @@ export default function RootLayout({
     </html>
   );
 }
+yarn add @vercel/analytics
+import { Analytics } from '@vercel/analytics/next';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
+vercel deploy
