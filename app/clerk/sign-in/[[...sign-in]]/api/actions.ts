b429032cd2e2ead085066f39a4f6e9bdf4fd714e
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-14 11:29:51 +0200

    Update actions.ts
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

--- app/clerk/sign-in/[[...sign-in]]/api/actions.ts
+++ app/clerk/sign-in/[[...sign-in]]/api/actions.ts
@@ -69,3 +69,33 @@ export async function addItem(formData: FormData) {
 
   console.log('add item server action', formData)
 }
+'use server'
+import { auth } from '@clerk/nextjs/server'
+
+export async function addItem(formData: FormData) {
+  const { userId } = await auth()
+
+  if (!userId) {
+    throw new Error('You must be signed in to add an item to your cart')
+  }
+
+  console.log('add item server action', formData)
+}
+'use server'
+import { currentUser } from '@clerk/nextjs/server'
+
+export async function addHobby(formData: FormData) {
+  const user = await currentUser()
+
+  if (!user) {
+    throw new Error('You must be signed in to use this feature')
+  }
+
+  const serverData = {
+    usersHobby: formData.get('hobby'),
+    userId: user.id,
+    profileImage: user.imageUrl,
+  }
+
+  console.log('add Hobby completed with user details ', serverData)
+}
