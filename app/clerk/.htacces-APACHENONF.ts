3bf68c7cfa4fe26095d9d94dd4f18ff6b01bea90
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-15 20:41:40 +0200

    Update .htacces-APACHENONF.ts
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

--- app/clerk/.htacces-APACHENONF.ts
+++ app/clerk/.htacces-APACHENONF.ts
@@ -8,3 +8,18 @@ location /status {
     auth_basic           "Access to the staging site";
     auth_basic_user_file /etc/apache2/.htpasswd;
 }
+<VirtualHost *:443>
+  ServerName example.com
+  Redirect / https://www.example.com
+</VirtualHost>
+RedirectMatch ^/images/(.*)$ https://images.example.com/$1
+Redirect permanent / https://www.example.com
+# …acts the same as:
+Redirect 301 / https://www.example.com
+server {
+  listen 80;
+  server_name example.com;
+  return 301 $scheme://www.example.com$request_uri;
+}
+rewrite ^/images/(.*)$ https://images.example.com/$1 redirect;
+rewrite ^/images/(.*)$ https://images.example.com/$1 permanent;
