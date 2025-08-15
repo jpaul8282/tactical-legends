f4c748d7f9f5696760a32ffac6c28ea3c1ddda6b
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-15 20:52:46 +0200

    Create JSON-compression-dictonary.ts
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

---
+++ app/clerk/JSON-compression-dictonary.ts
@@ -0,0 +1,20 @@
+function a() {
+  console.log("Hello World!");
+}
+
+function b() {
+  console.log("I am here");
+}
+Use-As-Dictionary: match="/js/app.*.js"
+Accept-Encoding: gzip, br, zstd, dcb, dcz
+Available-Dictionary: :pZGm1Av0IEBKARczz7exkNYsZb8LzaMrV7J32a2fFG4=:
+Content-Encoding: dcb
+Vary: accept-encoding, available-dictionary
+Use-As-Dictionary: match="/js/app.*.js", id="dictionary-12345"
+Accept-Encoding: gzip, br, zstd, dcb, dcz
+Available-Dictionary: :pZGm1Av0IEBKARczz7exkNYsZb8LzaMrV7J32a2fFG4=:
+Dictionary-ID: "dictionary-12345"
+<link rel="compression-dictionary" href="/dictionary.dat" />
+Link: </dictionary.dat>; rel="compression-dictionary"
+Use-As-Dictionary: match="/js/app.*.js"
+
