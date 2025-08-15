ae3429851bb5f3b961ce614d978e9453a418776f
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-15 20:56:03 +0200

    Create DATA.TXT.DCB.ts
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

---
+++ app/clerk/DATA.TXT.DCB.ts
@@ -0,0 +1,7 @@
+echo -en '\xffDCB' > data.txt.dcb && \
+openssl dgst -sha256 -binary dictionary.txt >> data.txt.dcb && \
+brotli --stdout -D dictionary.txt data.txt >> data.txt.dcb
+echo -en '\x5e\x2a\x4d\x18\x20\x00\x00\x00' > data.txt.dcz && \
+openssl dgst -sha256 -binary dictionary.txt >> data.txt.dcz && \
+zstd -D dictionary.txt -f -o tmp.zstd data.txt && \
+cat tmp.zstd >> data.txt.dcz
