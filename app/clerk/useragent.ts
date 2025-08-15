7756af82a0554fc1133a13ec36fd3fa6e85c813a
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-15 21:41:28 +0200

    Create useragent.ts
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

---
+++ app/clerk/useragent.ts
@@ -0,0 +1,253 @@
+User-Agent: <product> / <product-version> <comment>
+console.log(window.navigator.userAgent);
+// Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:138.0) Gecko/20100101 Firefox/138.0
+alert(window.navigator.userAgent);
+// alerts "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0"
+if (navigator.userActivation.isActive) {
+  // proceed to request playing media, for example
+}
+if (navigator.userActivation.hasBeenActive) {
+  // proceed with auto-playing an animation, for example
+}
+if ("serviceWorker" in navigator) {
+  // Supported!
+}
+if ("mediaSession" in navigator) {
+  navigator.mediaSession.metadata = new MediaMetadata({
+    title: "Podcast Episode Title",
+    artist: "Podcast Host",
+    album: "Podcast Name",
+    artwork: [{ src: "podcast.jpg" }],
+  });
+}
+navigator.mediaCapabilities
+  .decodingInfo({
+    type: "file",
+    audio: {
+      contentType: "audio/mp3",
+      channels: 2,
+      bitrate: 132700,
+      samplerate: 5200,
+    },
+  })
+  .then((result) => {
+    console.log(
+      `This configuration is ${result.supported? "": "not "}supported,`,
+    );
+    console.log(`${result.smooth ? "" : "not "}smooth, and`);
+    console.log(`${result.powerEfficient ? "" : "not "}power efficient.`);
+  });
+if (navigator.maxTouchPoints > 1) {
+  // browser supports multi-touch
+}
+navigator.language; // "en-US"
+navigator.languages; // ["en-US", "zh-CN", "ja-JP"]
+const date = new Date("2012-05-24");
+
+const formattedDate = new Intl.DateTimeFormat(navigator.languages).format(date);
+const date = new Date("2012-05-24");
+
+const formattedDate = new Intl.DateTimeFormat(navigator.language).format(date);
+let workerList = [];
+
+for (let i = 0; i < window.navigator.hardwareConcurrency; i++) {
+  let newWorker = {
+    worker: new Worker("cpu-worker.js"),
+    inUse: false,
+  };
+  workerList.push(newWorker);
+}
+if ("credentials" in navigator) {
+  navigator.credentials.get({ password: true }).then((creds) => {
+    //Process the credentials.
+  });
+} else {
+  // Handle sign-in the way you did before.
+}
+if (!navigator.cookieEnabled) {
+  // The browser does not support or is blocking cookies from being set.
+}
+navigator. clipboard
+  .readText()
+  .then(
+    (clipText) => (document.querySelector(".clip-text").innerText = clipText),
+  );
+const paragraphs = document.querySelectorAll("p");
+// paragraphs[0] is the first <p> element
+// paragraphs[1] is the second <p> element, etc.
+alert(paragraphs[0].nodeName);
+# Python DOM example
+import xml.dom.minidom as m
+doc = m.parse(r"C:\Projects\Py\chap1.xml")
+doc.nodeName # DOM property of document object
+p_list = doc.getElementsByTagName("para")
+<body onload="console.log('Welcome to my home page!');">
+  …
+</body>
+<html lang="en">
+  <head>
+    <script>
+      // run this function when the document is loaded
+      window.onload = () => {
+        // create a couple of elements in an otherwise empty HTML page
+        const heading = document.createElement("h1");
+        const headingText = document.createTextNode("Big Head!");
+        heading.appendChild(headingText);
+        document.body.appendChild(heading);
+      };
+    </script>
+  </head>
+  <body></body>
+</html>
+const table = document.getElementById("table");
+const tableAttrs = table.attributes; // Node/Element interface
+for (const attr of tableAttrs) {
+  // HTMLTableElement interface: border attribute
+  if (attr.nodeName.toLowerCase() === "border") {
+    table.border = "1";
+  }
+}
+// HTMLTableElement interface: summary attribute
+table.summary = "note: increased border";
+<div class="container">
+  <textarea class="story"></textarea>
+  <button id="set-text" type="button">Set text content</button>
+  <button id="clear-text" type="button">Clear text content</button>
+</div>
+.container {
+  display: flex;
+  gap: 0.5rem;
+  flex-direction: column;
+}
+
+button {
+  width: 200px;
+}
+const story = document.body.querySelector(".story");
+
+const setText = document.body.querySelector("#set-text");
+setText.addEventListener("click", () => {
+  story.textContent = "It was a dark and stormy night...";
+});
+
+const clearText = document.body.querySelector("#clear-text");
+clearText.addEventListener("click", () => {
+  story.textContent = "";
+});
+<div class="container">
+  <div class="parent">parent</div>
+  <button id="add-child" type="button">Add a child</button>
+  <button id="remove-child" type="button">Remove child</button>
+</div>
+.container {
+  display: flex;
+  gap: 0.5rem;
+  flex-direction: column;
+}
+
+button {
+  width: 100px;
+}
+
+div.parent {
+  border: 1px solid black;
+  padding: 5px;
+  width: 100px;
+  height: 100px;
+}
+
+div.child {
+  border: 1px solid red;
+  margin: 10px;
+  padding: 5px;
+  width: 80px;
+  height: 60px;
+  box-sizing: border-box;
+}
+const parent = document.body.querySelector(".parent");
+
+const addChild = document.body.querySelector("#add-child");
+addChild.addEventListener("click", () => {
+  // Only add a child if we don't already have one
+  // in addition to the text node "parent"
+  if (parent.childNodes.length > 1) {
+    return;
+  }
+  const child = document.createElement("div");
+  child.classList.add("child");
+  child.textContent = "child";
+  parent.appendChild(child);
+});
+
+const removeChild = document.body.querySelector("#remove-child");
+removeChild.addEventListener("click", () => {
+  const child = document.body.querySelector(".child");
+  parent.removeChild(child);
+});
+<html lang="en">
+  <head>
+    <title>My Document</title>
+  </head>
+  <body>
+    <h1>Header</h1>
+    <p>Paragraph</p>
+  </body>
+</html>
+<html lang="en">
+  <head>
+    <title>My Document</title>
+  </head>
+  <body>
+    <input type="button" value="Change this document." />
+    <h2>Header</h2>
+    <p>Paragraph</p>
+  </body>
+</html>
+document.querySelector("input").addEventListener("click", () => {
+  // document.getElementsByTagName("h2") returns a NodeList of the <h2>
+  // elements in the document, and the first is number 0:
+  const header = document.getElementsByTagName("h2").item(0);
+
+  // The firstChild of the header is a Text node:
+  header.firstChild.data = "A dynamic document";
+
+  // Now header is "A dynamic document".
+
+  // Access the first paragraph
+  const para = document.getElementsByTagName("p").item(0);
+  para.firstChild.data = "This is the first paragraph.";
+
+  // Create a new Text node for the second paragraph
+  const newText = document.createTextNode("This is the second paragraph.");
+
+  // Create a new Element to be the second paragraph
+  const newElement = document.createElement("p");
+
+  // Put the text in the paragraph
+  newElement.appendChild(newText);
+
+  // Put the paragraph on the end of the document by appending it to
+  // the body (which is the parent of para)
+  para.parentNode.appendChild(newElement);
+});
+const root = document.createElement("html");
+root.lang = "en";
+
+const head = document.createElement("head");
+const title = document.createElement("title");
+title.appendChild(document.createTextNode("My Document"));
+head.appendChild(title);
+
+const body = document.createElement("body");
+const header = document.createElement("h1");
+header.appendChild(document.createTextNode("Header"));
+const paragraph = document.createElement("p");
+paragraph.appendChild(document.createTextNode("Paragraph"));
+body.appendChild(header);
+body.appendChild(paragraph);
+
+root.appendChild(head);
+root.appendChild(body);
+
+
+
