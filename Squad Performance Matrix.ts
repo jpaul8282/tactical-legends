5911fc77c2cef4865661b026ccbda99d3c2bbd5c
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-25 16:51:35 +0200

    Create Squad Performance Matrix.ts
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

---
+++ Squad Performance Matrix.ts
@@ -0,0 +1,86 @@
+@startjson
+  <style>
+jsonDiagram {
+  node {
+    Background Color Khaki
+    LineColor lightblue
+    FontName Helvetica
+    FontColor red
+    FontSize 18
+    FontStyle bold
+    RoundCorner 0
+    LineThickness 2
+    LineStyle 10-5
+    separator {
+      LineThickness 0.5
+      LineColor black
+      LineStyle 1-5
+    }
+  }
+  arrow {
+    BackGroundColor lightblue
+    LineColor green
+    LineThickness 2
+    LineStyle 2-5
+  }
+  highlight {
+    BackGroundColor red
+    FontColor white
+    FontStyle italic
+  }
+}
+</style>
+#highlight "1" / "hr"
+[
+  {
+    "callSign": "Echo-27",
+    "name": "John Smith",
+    "faction": "Oistarian Vanguard",
+    "specialty": "Vault Phase, Tactical Recall",
+    "hr": 65,
+    "avg": 0.278,
+    "morale": 78,
+    "status": "Active"
+  },
+  {
+    "callSign": "Gunwafa",
+    "name": "Gunwafa El-Rami",
+    "faction": "IDF",
+    "specialty": "Urban Siege, Adaptive Combat",
+    "hr": 63,
+    "avg": 0.288,
+    "morale": 82,
+    "status": "Active"
+  },
+  {
+    "callSign": "Nyla Sera",
+    "name": "Nyla Sera",
+    "faction": "Vaultborn",
+    "specialty": "Recon, Memory Echo",
+    "hr": 58,
+    "avg": 0.265,
+    "morale": 65,
+    "status": "Active"
+  },
+  {
+    "callSign": "Korr Vex",
+    "name": "Korr Vex",
+    "faction": "Synth Ascendancy (Defector)",
+    "specialty": "Heavy Weapons, Terrain Manipulation",
+    "hr": 61,
+    "avg": 0.251,
+    "morale": 58,
+    "status": "Active"
+  },
+  {
+    "callSign": "Oistarian",
+    "name": "Unknown",
+    "faction": "Eden Core",
+    "specialty": "Psychic Shield, Relic Interface",
+    "hr": 55,
+    "avg": 0.295,
+    "morale": 70,
+    "status": "Active"
+  }
+]
+@endjson
