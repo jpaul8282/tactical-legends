e0fc7bd3ad87b5b84a4d258ed85b1b11191e838c
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-25 16:07:42 +0200

    Update MissionManager.cs
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

--- MissionManager.cs
+++ MissionManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class MissionManager : MonoBehaviour
+public class MissionManager: MonoBehaviour
 {
     public List<Mission> Missions;
 
@@ -21,3 +21,69 @@ public class MissionManager : MonoBehaviour
         GameManager.Instance.Player.AddReward(Missions[missionId].Reward);
     }
 }
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MissionManager: MonoBehaviour
+{
+    public string missionId = "vault_breathes";
+    public List<string> objectives = new List<string> {
+        "Navigate corridors",
+        "Disable AI sentinels",
+        "Extract Surge Protocol"
+    };
+
+    public List<string> enemies = new List<string> {
+        "AI Sentinel", "Memory Wraith"
+    };
+
+    public GameState gameState;
+
+    void Start()
+    {
+        gameState = new GameState();
+        gameState.InitializeSquad();
+        StartMission();
+    }
+
+    void StartMission()
+    {
+        Debug.Log("Mission Started: " + missionId);
+        foreach (string obj in objectives)
+            Debug.Log("Objective: " + obj);
+
+        EventManager.TriggerRandomEvent(gameState);
+        MoraleManager.CheckMorale(gameState.squad);
+        DialogueManager.PlayDialogue(gameState.squad);
+    }
+}
+
+using System.Collections.Generic;
+
+public class GameState
+{
+    public int trustScore = 65;
+    public int moralityScore = 80;
+    public List<SquadMember> squad = new List<SquadMember>();
+
+    public void InitializeSquad()
+    {
+        squad.Add(new SquadMember("Echo Vanguard", 85));
+        squad.Add(new SquadMember("Oistarian", 45));
+    }
+}
+
+public class SquadMember
+{
+    public string name;
+    public int morale;
+    public string status;
+
+    public SquadMember(string name, int morale)
+    {
+        this.name = name;
+        this.morale = morale;
+        this.status = "Active";
+    }
+}
+
