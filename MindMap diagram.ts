bad7efe40b50260b4e006964e3798229b0a5b8f6
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-25 17:56:25 +0200

    Create MindMap diagram.ts
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

---
+++ MindMap diagram.ts
@@ -0,0 +1,87 @@
+@startmindmap
+* Debian
+** Ubuntu
+*** Linux Mint
+*** Kubuntu
+*** Lubuntu
+*** KDE Neon
+** LMDE
+** SolydXK
+** SteamOS
+** Raspbian with a very long name
+*** <s>Raspmbc</s> => OSMC
+*** <s>Raspyfi</s> => Volumio
+@endmindmap
+
+@startmindmap
+* root node
+	* some first-level node
+		* second-level node
+		* another second-level node
+	* another first-level node
+@endmindmap
+
+@startmindmap
++ OS
+++ Ubuntu
++++ Linux Mint
++++ Kubuntu
++++ Lubuntu
++++ KDE Neon
+++ LMDE
+++ SolydXK
+++ SteamOS
+++ Raspbian
+-- Windows 95
+-- Windows 98
+-- Windows NT
+--- Windows 8
+--- Windows 10
+@endmindmap
+@startmindmap
+* Class Templates
+**: Example 1
+<code>
+template <typename T>
+class cname{
+void f1()<U+003B>
+...
+}
+</code>
+;
+**: Example 2
+<code>
+other template <typename T>
+class cname{
+...
+</code>
+;
+@endmindmap
+@startmindmap
++ root
+**:right_1.1
+right_1.2;
+++ right_2
+
+left side
+
+-- left_1
+-- left_2
+**:left_3.1
+left_3.2;
+@endmindmap
+@startmindmap
+* Root 1
+** Foo
+** Bar
+* Root 2
+** Lorem
+** Ipsum
+@endmindmap
+@startmindmap
+*[#Orange] Colors
+**[#lightgreen] Green
+**[#FFBBCC] Rose
+**[#lightblue] Blue
+@endmindmap
+
