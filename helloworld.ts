1534c0547febbc83620926d07d4a56fcad72a0d8
Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>
2025-08-25 16:46:42 +0200

    Create helloworld.ts
    Signed-off-by: Joachim paul westerveld <126770103+jurgen-paul@users.noreply.github.com>

---
+++ helloworld.ts
@@ -0,0 +1,22 @@
+@startjson.
+  {
+  "transmissionID": "TL-HELLO-0001",
+  "origin": "Vault Beacon Node 7",
+  "timestamp": "2525-08-25T16:44:00Z",
+  "sender": {
+    "callSign": "Echo-27",
+    "faction": "Oistarian Vanguard"
+  },
+  "message": {
+    "type": "EncryptedBroadcast",
+    "content": "Hello world!",
+    "priority": "Low",
+    "status": "Declassified"
+  },
+  "routing": {
+    "targetSector": "Eden-Prime",
+    "relayNodes": ["Grid-Alpha", "Vault-Spire", "IDF Command"]
+  }
+}
+
+@endjson
